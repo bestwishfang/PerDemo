8c8600485568128695c1f1bdfeccdb245425dae3
bestwishfang <bestwishfang@126.com>
2019-08-22 19:47:14 +0800

    based on pymysql programming

--- second.py
+++ second.py
@@ -1,7 +1,28 @@
-import time
-
-print(time.time())
+"""
 li = [1, 34, 56, 78, 34, 23, 34, 56]
 new_list = [x for x in li if li.count(x) == 1]
 print(new_list)
 print ("我是你项目的仰慕者")
+"""
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1',
+                       port=3306,
+                       user='root',
+                       password='123456',
+                       database='demo',
+                       )
+cursor = conn.cursor()
+
+sql = "select * from good;"
+
+row = cursor.execute(sql)
+conn.commit()
+
+print(row)
+# print(type(cursor.fetchall()))  # tuple
+for x in cursor.fetchall():
+    print(x)
+
+cursor.close()
+conn.close()
