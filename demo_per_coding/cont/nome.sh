604da63e5622b488a38997f01d6d6571e672e72e
bestwishfang <54384270+bestwishfang@users.noreply.github.com>
2020-11-19 11:47:02 +0800

    person note pytest

--- demo_per_coding/cont/nome.sh
+++ demo_per_coding/cont/nome.sh
@@ -1,1 +1,22 @@
 echo "New Starting..."
+# pytest.ini
+[pytest]
+addopts =
+python_files = 
+python_functions = 
+python_classes = 
+
+
+#!/bin/bash
+systemctl start docker.service
+systemctl start smb.service
+
+docker login 100.95.233.94 -u admin -pHarbor12345
+modprobe tipc
+modprobe squashfs
+
+umount /opt/home/Software
+mount "//hghrnd-fs/hgh01/DCP_Galaxy_GX_F/br_scp_hssp_master/current/MATRIX" /opt/home/Software -o username=用户名,password=密码,rw --verbose
+
+cd /opt/home/
+nohup swift &
