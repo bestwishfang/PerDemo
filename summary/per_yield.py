f6dce758009d706f0ea28eeb7576c5880ace40a6
bestwishfang <54384270+bestwishfang@users.noreply.github.com>
2021-03-06 17:17:19 +0800

    Recent Learn yield

---
+++ summary/per_yield.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import random
+
+
+def producer():
+    while True:
+        num = random.randint(10, 100)
+        print(f'Product: {num}')
+        yield num
+
+
+def consumer():
+    while True:
+        x = yield
+        print(f'Consume: {x}')
+
+
+if __name__ == '__main__':
+    p = producer()
+    c = consumer()
+    c.__next__()
+    for j in p:
+        c.send(j)
